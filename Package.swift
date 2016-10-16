6bc5777154c3fd68cc5c9ee5f087505ef813d1b8
oheroj <1814345797@qq.com>
2016-10-16 09:44:19 +0800

    Feat: 去掉数据库连接

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "spider",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor:1),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
