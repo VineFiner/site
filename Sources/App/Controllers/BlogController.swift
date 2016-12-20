b71df9e60f61b8e15ed68f36b1c9463049dcf029
oheroj <1814345797@qq.com>
2016-12-21 00:37:00 +0800

    Feat: 调整页面，实现文章的修改功能

--- Sources/App/Controllers/BlogController.swift
+++ Sources/App/Controllers/BlogController.swift
@@ -19,12 +19,34 @@ final class BlogController : BaseController {
         group.get(Post.self, handler: postDetail)
         group.post(Post.self, "delete", handler: deletePost) //delete
         group.get("create", handler: createPost) // create 页面
+        group.get("update", Post.self, handler:rePost)
+        group.post("update", Int.self, handler: updatePost)
     }
 
     func postDetail(request: Request, post: Post) throws -> ResponseRepresentable {
         let content = post.content
         let params = ["content": content]
         return try drop.view.make("blog/detail", params)
+    }
+
+    func updatePost(request: Request, id:Int) throws -> ResponseRepresentable {
+        guard let title = request.data["title"]?.string,
+            let content = request.data["content"]?.string,
+            let category = request.data["category"]?.string else {
+                throw Abort.badRequest
+        }
+        var post = try Post.find(id)
+        post?.title = title
+        post?.category = category
+        post?.content = content
+        try post?.save()
+        return Response(redirect: "/blog")
+
+    }
+
+    func rePost(request: Request, post: Post) throws -> ResponseRepresentable {
+        let params = ["post": post]
+        return try drop.view.make("blog/create", params)
     }
 
     func createPost(request: Request) throws -> ResponseRepresentable {
