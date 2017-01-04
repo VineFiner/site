2c321902e2adfb115f090790723db96d88830502
oheroj <1814345797@qq.com>
2017-01-04 23:12:58 +0800

    Feat: 添加其他样式

--- Sources/App/Controllers/IndexController.swift
+++ Sources/App/Controllers/IndexController.swift
@@ -12,12 +12,19 @@ import HTTP
 final class IndexController : BaseController {
 
     override func addRouters() {
-        drop.get(handler: indexView)
+        drop.get("/",handler: indexView)
+        drop.get("post", Post.self, handler: detailView)
     }
 
     func indexView(request: Request) throws -> ResponseRepresentable {
         let posts = try Post.all().makeNode()
         let params = try Node(node: ["posts": posts])
         return try drop.view.make("index", params)
+    }
+
+    func detailView(request: Request, post: Post) throws -> ResponseRepresentable {
+        let params = try Node(node: ["post": post.makeNode()])
+        return try drop.view.make("detail", params)
+
     }
 }
