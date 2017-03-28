4992beb19bd4e2ab028bb9036faecd71e49cc1f3
oheroj <1814345797@qq.com>
2017-03-28 14:33:14 +0800

    Feat: 添加上一页，下一页

--- Sources/App/Controllers/IndexController.swift
+++ Sources/App/Controllers/IndexController.swift
@@ -15,28 +15,39 @@ final class IndexController : BaseController {
     override func addRouters() {
         drop.get("/",handler: indexView)
         drop.get("post", Post.self, handler: detailView)
-        drop.post("/", handler: pageView)
     }
 
     func indexView(request: Request) throws -> ResponseRepresentable {
-        let posts = try Post.all().makeNode()
-
-        let params = try Node(node: ["posts": posts])
-        return try drop.view.make("index", params)
-    }
-
-    // 分页
-    func pageView(request: Request) throws -> ResponseRepresentable {
-
-        guard let page = request.data["page"]?.int else{
-            throw Abort.badRequest
+        var page = 1
+        if let pageIndex = request.data["page"]?.int {
+            page = pageIndex
         }
+
         let pageCount = 5 // 每页显示条数
         // 获取到总条数
         let postsCount = try Post.all().count;
 
         // 总页数
         let pageNum = postsCount / pageCount + 1;
+
+        var hasNext = true //是否有下一页
+        var hasPre = true // 是否有上一页
+
+        if (page > pageNum) {
+            page = pageNum
+        }
+
+        if (page < 1) {
+            page = 1
+        }
+
+        if (page == pageNum) {
+            hasNext = false
+        }
+
+        if (1 == page) {
+            hasPre = false
+        }
 
         if (page > pageNum || page == 0) {
             throw Abort.badRequest
@@ -51,7 +62,10 @@ final class IndexController : BaseController {
 
         let posts = try querty.all().makeNode()
 
-        return try JSON(node: ["count": postsCount, "posts": posts, "form": offset]).makeResponse()
+//        let params = try Node(node:["hasNext":hasNext, "hasPre": hasPre, "posts": posts, "currentPage":page])
+        let params = try Node(node:["posts": posts])
+
+        return try drop.view.make("index", params)
     }
 
     func detailView(request: Request, post: Post) throws -> ResponseRepresentable {
